cf9665c19dfe2f9df33114a742205ad94af1310c
IvanKhartov <ivan.khartov@gmail.com>
2015-09-11 15:41:50 +0300

    bottom part(section - app slider, app pave, pave send) done.

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,55 @@
 $(document).ready(function() {
     $(".owl-carousel").owlCarousel({
-        //items : 5,
+        items: 5,
         loop: true,
         center: true,
         margin: 10,
         nav: true,
+        responsiveClass:true,
         responsive:{
             0:{items:1},
             768:{items:1},
-            1025:{items:3},
-            1281:{items:5}
+            800:{items:1},
+            1024:{items:3},
+            1280:{items:5},
+            1680:{items:5}
+        }
+    });
+    $("#paveOs .counters > .row").owlCarousel({
+        items: 3,
+        dots:true,
+        responsiveClass:true,
+        responsive:{
+            0:{items:1},
+            750:{items:1},
+            768:{items:3, dots:false},
+            1024:{items:3, dots:false},
+            1680:{items:3, dots:false}
         }
     });
 });
+
+var txt = $('.writer').text();
+var timeOut;
+var txtLen = txt.length;
+var char = 0;
+
+$('#sendAppBtn').click(function(){
+    $('#sendAppBtn').addClass('hidden');
+    $('#sendForm').removeClass('hidden');
+    $('#sendApp').addClass('scale');
+    $('#sendForm .btn-send').animate({
+        right: "0%",
+        marginRight: "0"
+    }, 3000, function(){
+        $('#sendForm .input-sm').animate({
+            opacity: "1"
+        }, 800, function(){
+            $('#sendForm .input-sm').addClass('placeholder');
+        });
+    });
+});
+
 $('#search-btn').bind('click', function(){
     $('#search-block').animate({
         opacity: '1',
