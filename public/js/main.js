2f1ff7bad9e92384e6fda11ba718a78403d7bbac
kavkamisha <kavkamisha@gmail.com>
2015-10-01 20:22:55 +0300

    middle part

--- public/js/main.js
+++ public/js/main.js
@@ -185,4 +185,18 @@ $('#inBoxToggle').click(function(){
         }, 850);
     }
 });
+$(function() {
+    $('.item a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
